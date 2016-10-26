8ba31d75ed5490d5ba378324aae9f3c2271176e0
Gokhan Derala <gderala@gmail.com>
2016-10-27 00:03:58 +0300

    header

--- header.php
+++ header.php
@@ -22,7 +22,5 @@ echo "<title>$appname$userstr</title>
 if($loggedin)
 	echo "<br><ul class='menu'>"."<li><a href='members.php?view=$user'>Home</a></li>"."<li><a href='members.php'>Members</a></li>"."<li><a href='friends.php'>Friends</a></li>"."<li><a href='messages.php'>Messages</a></li>"."<li><a href='profile.php'>Edit Profile</a></li>"."<li><a href='logout.php'>Log out</a></li></ul><br>";
 else
-	echo ("<br><ul class='menu'>"."<li><a href='index.php'>Home</a></li>"."<li><a href='signup.php'>Sign up</a></li>".)
-
-
+	echo ("<br><ul class='menu'>"."<li><a href='index.php'>Home</a></li>"."<li><a href='signup.php'>Sign up</a></li>"."<li><a href='login.php'>Log in</a></li></ul><br>"."<span class='info'>&#8658; You must be logged in to "."view this page</span><br><br>");
 ?>
