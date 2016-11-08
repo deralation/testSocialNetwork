f4c9c5f2e748589445a3a459b597ec530e7d05fb
gokhanDerala <gderala@gmail.com>
2016-11-08 21:55:31 +0300

    members

--- members.php
+++ members.php
@@ -30,5 +30,36 @@ if(isset($_GET["add"])){
 	queryMysql("DELETE FROM friends WHERE user='$remove' AND friend='$user'");
 }
 
+$result = queryMysql("SELECT user FROM members ORDER BY user");
+$num = $result->num_rows;
 
+echo "<h3>Other Members</h3><ul>";
+
+for ($j=0; $j < $num ; $j++) { 
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	if($row['user'] == $user) continue;
+
+	echo "<li><a href='members.php?view".$row['user']."'>".$row['user']."</a>";
+
+	$follow = "follow";
+
+	$result1 = queryMysql("SELECT * FROM friends WHERE user='".$row['user']."' AND friend='$user'");
+
+	$t1 = $result1->num_rows;
+
+	$result1 = queryMysql("SELECT * FROM friends WHERE user='$user' AND friend='".$row['user']."'");
+	$t2 = $result1->num_rows;
+
+	if(($t1 + $t2)) echo "&harr; is a mutual friend";
+	elseif($t1)		echo "$larr; you are following";
+	elseif($t2)		echo "$rarr; is following you";
+		$follow = "recip";}
+
+	if(!$t1)	echo "[<a href='members.php?add=".$row['user']."'>$follow</a>]";
+	else 		echo "[<a href='members.php?remove=".$row['user']."'>drop</a>]";
+}
 ?>
+
+		</ul></div>
+	</body>
+</html>	
