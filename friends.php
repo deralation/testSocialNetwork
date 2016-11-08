09c612adc99f8fa99ae1aefb84879b6149cefae4
gokhanDerala <gderala@gmail.com>
2016-11-08 22:12:12 +0300

    friends

---
+++ friends.php
@@ -0,0 +1,36 @@
+<?php  
+require_once 'header.php';
+
+if(!$loggedin) die();
+
+if(isset($_GET['view'])){
+	$view = sanitizeString($_GET['view']);
+else $view = $user;
+
+if($view==$user){
+	$name1=$name2= "Your";
+	$name3 = "You are";
+}
+else
+{
+	$name1= "<a href='members.php?view=$view'>$view</a>'s";
+	$name2= "$view's";
+	$name3= "$view is";
+}
+
+echo "<div class='main'>";
+
+$followes = array();
+$following = array();
+
+$result = queryMysql("SELECT * FROM friends WHERE user='$view'");
+$num = $result->num_rows;
+
+for ($i=0; $i < $num ; $i++) { 
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	$followers[$j] = $row['friend']; 
+}
+
+
+
+?>
