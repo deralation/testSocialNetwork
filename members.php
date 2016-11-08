d211c0efe1108cc7fa406e96be926a1b3562a909
Gokhan Derala <gderala@gmail.com>
2016-11-08 10:43:49 +0200

    members query for add members

--- members.php
+++ members.php
@@ -15,8 +15,20 @@ if(isset($_GET['view'])){
 	showProfile($view);
 
 	echo "<a class='button' href='messages.php?view=$view'>"."view $name message</a><br><br>";
+	die("</div></body></html>");
 }
 
+if(isset($_GET["add"])){
+	$add = sanitizeString($_GET['add']);
+
+	$result = queryMysql("SELECT * FROM friends WHERE user='$add' AND friend=$user");
+
+	if(!$result->num_rows)
+		queryMysql("INSERT INTO friends VALUES ('$add', '$user')");
+}elseif(isset($_GET['remove'])){
+	$remove = sanitizeString($_GET['remove']);
+	queryMysql("DELETE FROM friends WHERE user='$remove' AND friend='$user'");
+}
 
 
 ?>
