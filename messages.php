f39e78944fe6da3b229550e88370e3d9c2d874e1
gokhanDerala <gderala@gmail.com>
2016-11-09 22:33:26 +0300

    messages

--- messages.php
+++ messages.php
@@ -1,5 +1,21 @@
 <?php  
 require_once "header.php";
 
+if(!$loggedin) die();
+
+if(isset($_GET['view'])) $view = sanitizeString($_GET['view']);
+else $view = $user;
+
+if($_POST['text']){
+	$text = sanitizeString($_POST['text']);
+
+	if($text != ""){
+		$pm = substr(sanitizeString($_POST['pm']), 0,1);
+
+		$time = time();
+		queryMysql("INSERT INTO messages VALUES(NULL, '$user','$view','$pm','$time','$text'");
+	}
+}
+
 
 ?>
