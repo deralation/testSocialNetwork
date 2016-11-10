a8e5dafba54d950de0a460d29d281c3d55ea7afa
gokhanDerala <gderala@gmail.com>
2016-11-10 22:11:59 +0300

    changes

--- messages.php
+++ messages.php
@@ -24,4 +24,26 @@ if($view != ""){
 	$name2 = "$view's";
 }
 
+echo "<div class='main'><h3>$name1 Messages</h3>";
+showProfile($view);
+
+echo <<<_END
+	<form method="post" action='messages.php?view=$view'>Type here to leave a message:<br>
+	<textarea name="text" cols='40' rows='3'></textarea><br>
+	Public<input type='radio' name='pm' value='0' checked='checked'>
+	Private<input type='radio' name='pm' value='1'>
+	<input type="submit" value="Post Message"></form><br>
+_END;
+
+if(isset($_GET['erase'])){
+	$erase = sanitizeString($_GET['erase']);
+	queryMysql("DELETE FROM messages WHERE id=$erase AND recip='$user'");
+}
+
+$query = "SELECT * FROM messages WHERE recip='$view' ORDER BY time DESC";
+
+$result = queryMysql($query);
+$num = $result->num_rows;
+
+
 ?>
